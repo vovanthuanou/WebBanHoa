5e2641d5beb858bad6a30aa522f637fe1603d99e
hieu123 <62754329+hieu123222@users.noreply.github.com>
2020-06-16 22:52:20 +0700

    Add files via upload

---
+++ WebBanHoa.Common/Rep/ChiNhanhReq.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebBanHoa.Common.BLL;
+using WebBanHoa.Common.DAL;
+
+namespace WebBanHoa.Common.Rep
+{
+    public class ChiNhanhReq
+    {
+        public int MaChiNhanh { get; set; }
+        public int? MaNv { get; set; }
+        public string TenChiNhanh { get; set; }
+        public string DiaChi { get; set; }
+        public DateTime? NgayHoatDong { get; set; }
+        public DateTime? GiaThue { get; set; }
+
+        public virtual NhanVienreq MaNvNavigation { get; set; }
+    }
+}
---
+++ WebBanHoa.Common/Rep/DeleteChiNhanhReq.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebBanHoa.Common.Rep
+{
+    public class DeleteChiNhanhReq
+    {
+        public int MaChiNhanh { get; set; }
+    }
+}
