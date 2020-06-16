52f8592725f69318501cab115336244b964af507
hieu123 <62754329+hieu123222@users.noreply.github.com>
2020-06-16 22:53:54 +0700

    Add files via upload

---
+++ WebBanHoa.Web/Controllers/ChiNhanhController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebBanHoa.BLL;
+using WebBanHoa.Common.Rep;
+using WebBanHoa.Common.Req;
+using WebBanHoa.Common.Rsp;
+
+namespace WebBanHoa.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ChiNhanhController : Controller
+    {
+
+        public ChiNhanhController()
+        {
+            _svc = new ChiNhanhSvc();
+        }
+        [HttpPost("lay-thong-tin-chi-nhanh-theo-machinhanh")]
+        public IActionResult getChiNhanhByMaChiNhanhbyId([FromBody] SimpleReq req)
+        {
+            var res = new SingleRsp();
+            res = _svc.Read(req.Id);
+            return Ok(res);
+        }
+
+        [HttpPost("get-all")]
+        public IActionResult getAllChiNhanhById()
+        {
+            var res = new SingleRsp();
+            res.Data = _svc.All;
+            return Ok(res);
+        }
+        [HttpPost("create-chinhanh")]
+        public IActionResult CreateChiNhanh([FromBody] ChiNhanhReq req)
+        {
+            var res = _svc.CreateChiNhanh(req);
+
+            return Ok(res);
+        }
+        [HttpPost("update-chinhanh")]
+        public IActionResult UpdateChiNhanh([FromBody] ChiNhanhReq req)
+        {
+            var res = _svc.CapnhatChiNhanh(req);
+
+            return Ok(res);
+        }
+        [HttpPost("delete-chinhanh")]
+        public IActionResult DeleteChiNhanh(DeleteChiNhanhReq req)
+        {
+            var res = _svc.DeleteChiNhanh(req.MaChiNhanh);
+
+            return Ok(res);
+        }
+        [HttpPost("search_chinhanh")]
+        public IActionResult SearchChiNhanhCtrl([FromBody] SearchChiNhanhReq req)
+        {
+            var res = new SingleRsp();
+            var pros = _svc.SearchChiNhanh(req.Page, req.Size, req.Keyword);
+            res.Data = pros;
+
+            return Ok(res);
+        }
+
+        private readonly ChiNhanhSvc _svc;
+    }
+
+}
