a0c1b8f7753d067617d8dce00fdbe1ce91a3ae24
hieu123222 <betkham134@gmail.com>
2020-06-21 14:12:01 +0700

    add frontend

--- WebBanHoa.Web/ClientApp/src/app/app.module.ts
+++ WebBanHoa.Web/ClientApp/src/app/app.module.ts
@@ -1,31 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { ProductsComponent } from "./products/products.component";
+import { AppComponent } from "./app.component";
+import { CartComponent } from "./cart/cart.component";
+import { HeaderComponent } from "./share/header/header.component";
+import { LoginComponent } from "./share/login/login.component";
+import { HomeComponent } from "./home/home.component";
+import { FooterComponent} from "./share/footer/footer.component";
+import { CounterComponent } from "./counter/counter.component";
+import { FetchDataComponent } from "./fetch-data/fetch-data.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
+    LoginComponent,
+    HeaderComponent,
+    ProductsComponent,
+    FooterComponent,
+    CartComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: "", component: HomeComponent, pathMatch: "full" },
+
+      { path: "login", component: LoginComponent },
+      
+      { path: "product-detail", component: ProductsComponent },
+
+      // Access for Registered Users
+      { path: "shopping-cart", component: CartComponent },
+
+
     ])
   ],
   providers: [],
